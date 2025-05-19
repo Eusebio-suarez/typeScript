7a1044a1bbc9149081b91a14f8463ca18eb70a47
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-05-18 21:37:01 -0500

    obtener informacion completa y cambio en la organizacion

--- dist/scripts/citas.js
+++ dist/scripts/citas.js
@@ -6,9 +6,9 @@ const contenedorCitas = document.getElementById("citasContainer");
 export function renderCitas() {
     citas.forEach(cita => {
         const cardCita = document.createElement("div");
-        cardCita.className = " group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative";
+        cardCita.className = "flex group min-w-full hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center items-center shadow-xl rounded-lg p-5 relative gap-3";
         cardCita.innerHTML = `
-        <button class="bg-amber-700 absolute top-1.5 right-1.5 btn-editar" data-id="${cita.id}" >
+        <button class="bg-amber-700 absolute top-1 right-1 btn-editar" data-id="${cita.id}" >
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#eb0505"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit absolute top-1.5 right-1.5 opacity-0 group-hover:opacity-100"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
         </button>
             <p><strong>ID:</strong>${cita.id}</p>
@@ -17,6 +17,7 @@ export function renderCitas() {
            <p>👤${cita.paciente}</p>
            <p>🧑‍⚕️${cita.medico}</p>
            <p>🏢-${cita.numConsultorio}</p>
+           <p>📝${cita.motivo}</p>
         `;
         if (cita.estado.toLowerCase() == "pendiente") {
             cardCita.innerHTML += `<p class="text-[#5F6368]">🟡${cita.estado}</p>`;
@@ -52,7 +53,7 @@ function buscarCita() {
     citas.forEach(cita => {
         if (input.value == cita.id.toString() || input.value == cita.paciente) {
             const cardCita = document.createElement("div");
-            cardCita.className = " group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative";
+            cardCita.className = " flex p-6 min-w-full items-cemter gap-3 group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative";
             cardCita.innerHTML = `
             <button class="bg-amber-700 absolute top-1.5 right-1.5 btn-editar" data-id="${cita.id}" >
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#eb0505"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit absolute top-1.5 right-1.5 opacity-0 group-hover:opacity-100"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
@@ -61,6 +62,9 @@ function buscarCita() {
             <p class="text-blue-500">📅${cita.fecha}</p>
             <p class="text-emerald-400">⏰${cita.hora}</p>
             <p>👤${cita.paciente}</p>
+            <p>🧑‍⚕️${cita.medico}</p>
+            <p>🏢-${cita.numConsultorio}</p>
+            <p>📝${cita.motivo}</p>
             `;
             if (cita.estado.toLowerCase() == "pendiente") {
                 cardCita.innerHTML += `<p class="text-[#5F6368]">🟡${cita.estado}</p>`;
@@ -83,6 +87,7 @@ function buscarCita() {
 }
 //funcion para actualizar cita
 export function buscarInformacionCita(id) {
+    //obtener elementos del DOOM
     const elementoId = document.getElementById("id");
     const elementoFecha = document.getElementById("fecha");
     const elementoHora = document.getElementById("hora");
@@ -90,12 +95,14 @@ export function buscarInformacionCita(id) {
     const elementoMedico = document.getElementById("medico");
     const elementoConsultorio = document.getElementById("consultorio");
     const elementoEstado = document.getElementById("estado");
+    const elementoMotivo = document.getElementById("motivo");
     const cita = citas[id - 1];
-    elementoId.textContent = cita.id.toString();
+    elementoId.textContent = "ID:" + cita.id.toString();
     elementoFecha.value = cita.fecha;
     elementoHora.value = cita.hora;
     elementoPaciente.value = cita.paciente;
     elementoMedico.value = cita.medico;
     elementoConsultorio.value = cita.numConsultorio.toString();
     elementoEstado.value = cita.estado;
+    elementoMotivo.value = cita.motivo;
 }
--- src/scripts/citas.ts
+++ src/scripts/citas.ts
@@ -8,9 +8,9 @@ const contenedorCitas = document.getElementById("citasContainer") as HTMLDivElem
 export function renderCitas():void{
     citas.forEach(cita =>{
         const cardCita = document.createElement("div") as HTMLDivElement
-        cardCita.className=" group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative"
+        cardCita.className="flex group min-w-full hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center items-center shadow-xl rounded-lg p-5 relative gap-3"
         cardCita.innerHTML=`
-        <button class="bg-amber-700 absolute top-1.5 right-1.5 btn-editar" data-id="${cita.id}" >
+        <button class="bg-amber-700 absolute top-1 right-1 btn-editar" data-id="${cita.id}" >
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#eb0505"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit absolute top-1.5 right-1.5 opacity-0 group-hover:opacity-100"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
         </button>
             <p><strong>ID:</strong>${cita.id}</p>
@@ -19,6 +19,7 @@ export function renderCitas():void{
            <p>👤${cita.paciente}</p>
            <p>🧑‍⚕️${cita.medico}</p>
            <p>🏢-${cita.numConsultorio}</p>
+           <p>📝${cita.motivo}</p>
         `
         if(cita.estado.toLowerCase()=="pendiente"){
             cardCita.innerHTML+=`<p class="text-[#5F6368]">🟡${cita.estado}</p>`
@@ -59,7 +60,7 @@ function buscarCita():void{
     citas.forEach(cita => {
         if(input.value==cita.id.toString()||input.value==cita.paciente){
             const cardCita = document.createElement("div") as HTMLDivElement
-            cardCita.className=" group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative"
+            cardCita.className=" flex p-6 min-w-full items-cemter gap-3 group w-[230px] hover:scale-[103%] hover:border-b-[3px] border-[#0077b6] transition duration-300 text-center shadow-xl rounded-lg p-4 relative"
             cardCita.innerHTML=`
             <button class="bg-amber-700 absolute top-1.5 right-1.5 btn-editar" data-id="${cita.id}" >
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#eb0505"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit absolute top-1.5 right-1.5 opacity-0 group-hover:opacity-100"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
@@ -68,6 +69,9 @@ function buscarCita():void{
             <p class="text-blue-500">📅${cita.fecha}</p>
             <p class="text-emerald-400">⏰${cita.hora}</p>
             <p>👤${cita.paciente}</p>
+            <p>🧑‍⚕️${cita.medico}</p>
+            <p>🏢-${cita.numConsultorio}</p>
+            <p>📝${cita.motivo}</p>
             `
             if(cita.estado.toLowerCase()=="pendiente"){
                 cardCita.innerHTML+=`<p class="text-[#5F6368]">🟡${cita.estado}</p>`
@@ -93,21 +97,23 @@ function buscarCita():void{
 
 //funcion para actualizar cita
 export function buscarInformacionCita(id: number): void {
-  const elementoId = document.getElementById("id") as HTMLSpanElement;
-  const elementoFecha = document.getElementById("fecha") as HTMLInputElement;
-  const elementoHora = document.getElementById("hora") as HTMLInputElement;
-  const elementoPaciente = document.getElementById("paciente") as HTMLInputElement;
-  const elementoMedico = document.getElementById("medico") as HTMLInputElement;
-  const elementoConsultorio = document.getElementById("consultorio") as HTMLInputElement;
-  const elementoEstado = document.getElementById("estado") as HTMLSelectElement;
+    //obtener elementos del DOOM
+    const elementoId = document.getElementById("id") as HTMLSpanElement;
+    const elementoFecha = document.getElementById("fecha") as HTMLInputElement;
+    const elementoHora = document.getElementById("hora") as HTMLInputElement;
+    const elementoPaciente = document.getElementById("paciente") as HTMLInputElement;
+    const elementoMedico = document.getElementById("medico") as HTMLInputElement;
+    const elementoConsultorio = document.getElementById("consultorio") as HTMLInputElement;
+    const elementoEstado = document.getElementById("estado") as HTMLSelectElement;
+    const elementoMotivo= document.getElementById("motivo") as HTMLInputElement
+    const cita = citas[id - 1];
 
-  const cita = citas[id - 1];
-
-  elementoId.textContent = cita.id.toString();
-  elementoFecha.value = cita.fecha;
-  elementoHora.value = cita.hora;
-  elementoPaciente.value = cita.paciente;
-  elementoMedico.value = cita.medico;
-  elementoConsultorio.value = cita.numConsultorio.toString();
-  elementoEstado.value = cita.estado;
+    elementoId.textContent ="ID:"+cita.id.toString();
+    elementoFecha.value = cita.fecha;
+    elementoHora.value = cita.hora;
+    elementoPaciente.value = cita.paciente;
+    elementoMedico.value = cita.medico;
+    elementoConsultorio.value = cita.numConsultorio.toString();
+    elementoEstado.value = cita.estado;
+    elementoMotivo.value=cita.motivo
 }
